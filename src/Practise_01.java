f065fa759e945150ebaa7f049dfa6317d6ddde53
hadi.hussain <hadi.hussain@elev.ga.ntig.se>
2024-04-17 10:58:49 +0200

    Initial commit

---
+++ src/Practise_01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Practise_01 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ange användarnamn:  ");
+        String username = scanner.nextLine();
+
+        if (username.equals("noname"));
+        System.out.println("Welcome");
+    }
+}
